4ccceac37ee94276b87912d7ff074bc476cf208e
AgustinDeArrechea <dearrecheaagustin@gmail.com>
2024-11-25 19:29:34 -0300

    .

---
+++ js/categories.js
@@ -0,0 +1,15 @@
+function cargarCategorias(){
+    fetch('https://dummyjson.com/recipes/tags')
+        .then(function(respuesta){
+            return respuesta.json();
+        })
+        .then(function(Categorias){
+            let categ= 
+
+
+  } )
+
+
+
+
+}
