2bfb55c72201146404514a25e1ba7804ffe7608a
MateoPerez2 <mateopaparella@hotmail.com>
2024-11-25 12:24:02 -0300

    Agregar 10 recetas

---
+++ js/index.js
@@ -0,0 +1,38 @@
+
+
+// Función para cargar recetas desde la API
+function cargarRecetas() {
+    fetch(`https://dummyjson.com/recipes?limit=10&skip=10`)
+        .then(function(respuesta) {
+            return respuesta.json();
+        })
+        .then(function(informacion) {
+            let info = informacion.recipes;
+            console.log(info);
+            let lista = document.querySelector("#campo-recetas");
+            for (let i = 0; i < info.length; i++) {
+                let receta = `
+                    <div class="receta">
+                        <a href="receta.html?id=${info[i].id}">
+                            <img src="${info[i].image}"">
+                            <h3>${info[i].name}</h3>
+                            <p>Categorías: ${info[i].tags}</p>
+                            <p>Dificultad: ${info[i].difficulty}</p>
+                        </a>
+                    </div>
+                `;
+                lista.innerHTML += receta;
+            }
+        })
+        .catch(function(error) {
+            console.log('El error es: ' + error);
+        });
+}
+
+cargarRecetas();
+
+let cargarMasBtn = document.getElementById('cargar-mas');
+
+cargarMasBtn.addEventListener('click', function() {
+    cargarRecetas();
+});
