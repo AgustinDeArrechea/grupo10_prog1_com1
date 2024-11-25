df46b60f08a0d5c21dd12dec155e72f57a1c04fe
AgustinDeArrechea <dearrecheaagustin@gmail.com>
2024-11-25 19:56:59 -0300

    categories.js

--- js/categories.js
+++ js/categories.js
@@ -8,6 +8,18 @@ function cargarCategorias(){
         })
         .then(function(categorias) {
             console.log(categorias) 
+            let info = categorias
+            let lista = document.querySelector("#Campo-Categorias");
+            for (let i = 0; i < categorias.length; i++) {
+                let categ = `
+                    <div class="receta">
+                        <a href="category.html?id=${categorias[i]}">
+                            <p>${categorias[i]}</p>
+                        </a>
+                    </div>
+                `;
+                lista.innerHTML += categ;
+            }
         })
         .catch(function(error){
             console.log("El error es: " + error);
