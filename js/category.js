1c97d70d4f2186c2e3fc19db0af6fe4d514e4a71
MateoPerez2 <mateopaparella@hotmail.com>
2024-11-28 00:26:24 -0300

    svgs

--- js/category.js
+++ js/category.js
@@ -19,14 +19,12 @@ function cargarCategorias(){
             for (let i = 0; i < categorias.recipes.length; i++) {
                 let categ = `
                     <div class="receta">
-                        <a href="receta.html?id=${categorias.recipes[i].id}" >
-                            <img class="img-index" src="${categorias.recipes[i].image} "class="imgReceta">
-                            <div class="receta-text"> 
-                            <h3>${categorias.recipes[i].name}</h3>
-                            <p>Categorías: ${categorias.recipes[i].tags}</p>
-                            <p>Dificultad: ${categorias.recipes[i].difficulty}</p> 
-                            </div>
-                        </a>
+                        <img class="img-index" src="${categorias.recipes[i].image} "class="imgReceta">
+                        <div class="receta-text"> 
+                        <h3>${categorias.recipes[i].name}</h3>
+                        <p>Dificultad: ${categorias.recipes[i].difficulty}</p>
+                        <a href="receta.html?id=${categorias.recipes[i].id}" >Ver detalles <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#000000"><path d="M216-144q-29.7 0-50.85-21.15Q144-186.3 144-216v-528q0-29.7 21.15-50.85Q186.3-816 216-816h264v72H216v528h528v-264h72v264q0 29.7-21.15 50.85Q773.7-144 744-144H216Zm171-192-51-51 357-357H576v-72h240v240h-72v-117L387-336Z"/></svg></a>
+                        </div>
                     </div>
                 `;
                 lista.innerHTML += categ;
